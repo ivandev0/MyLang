a31184cf78e43ed82c0a6feed8764d69027dd29a
Ivan Kylchik <cilcic.v22@gmail.com>
2018-10-06 18:41:57 +0300

    Прописал build.gradle файл

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,31 @@
-plugins {
-    java
-}
+val generateGrammarSource: AntlrTask by tasks
 
-version = "1.0-SNAPSHOT"
+plugins {
+    idea
+    java
+    antlr
+}
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
+    antlr( "org.antlr", "antlr4", "4.7.1")
+    compile("org.antlr", "antlr4-runtime", "4.7.1")
     testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+generateGrammarSource.apply {
+    outputDirectory = file("${project.projectDir}/src/main/java/antlr/")
+    arguments.addAll(listOf("-Dlanguage=Java", "-visitor", "-package", "antlr"))
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn(generateGrammarSource)
 }
